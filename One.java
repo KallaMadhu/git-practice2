6f508e53375f85ad300ce0c2cd6654d3aec4fcd6
shekar <kallamadhu10@gmail.com>
2023-12-27 11:31:50 +0530

    V1.0 Commit On One.java file

--- One.java
+++ One.java
@@ -1,10 +1,9 @@
 public class One
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         int a=10,b=2,c=0;
         c=a+b;
-        System.out.println(c);
-
-
+        System.out.println("c:\t"+c);
     }
 }
