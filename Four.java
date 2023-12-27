76182b51febcc44ff1280363cb95bef5435017ef
shekar <kallamadhu10@gmail.com>
2023-12-27 11:49:25 +0530

    Scanner object modification in Four.java v1.1

--- Four.java
+++ Four.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Four {
     public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
         System.out.println("Four manin.....");
     }
     
